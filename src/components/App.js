8e52fb8fb0179fd4d4755d7af45d78d760a641f9
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-04-03 13:41:45 +0700

    react-mobx-couter-sample

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,42 @@
 import React from "react";
+import { observable, computed } from "mobx";
+import { observer } from "mobx-react";
 
-const App = () => <div>App Component</div>;
+class AppState {
+  @observable count = 0;
+
+  @computed
+  get getCount() {
+    return this.count;
+  }
+
+  inc() {
+    this.count++;
+  }
+}
+
+@observer
+class Counter extends React.Component {
+  render() {
+    const { appState } = this.props;
+    return (
+      <div>
+        <p>counter: {appState.getCount}</p>
+        <br />
+        <button onClick={() => appState.inc()}>+</button>&nbsp;
+        <button onClick={() => (appState.count -= 1)}>-</button>
+      </div>
+    );
+  }
+}
+
+const store = new AppState();
+
+const App = () => (
+  <div>
+    <h2>Index Page</h2>
+    <Counter appState={store} />
+  </div>
+);
 
 export default App;
